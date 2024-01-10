f02ea3600d05a7f991951d2908087eb76e6e23a0
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-01-09 22:16:47 -0800

    continuously bridge w/ metric

--- hyperlane/user-emulator/entrypoint.sh
+++ hyperlane/user-emulator/entrypoint.sh
@@ -12,10 +12,6 @@ EOF
 
 echo ".dogrc file created successfully."
 
-# TODO: incorperate metric posting and continuous briding
-# dog --help
-# dog --config /.dogrc metric post testjan8 100
-
 # Fail script if no warp deployment file is found
 if [ ! -f /deploy-artifacts/warp-deployment.json ]; then
     echo "Error: warp-deployment.json not found. Please deploy bridge."
@@ -52,8 +48,30 @@ bridge-cli init \
     ${SEPOLIA_URL} ${MEV_COMMIT_CHAIN_URL} \
     --yes
 
-# Bridge to self on mev-commit chain
-bridge-cli bridge-to-mev-commit 890 $EMULATOR_ADDRESS $EMULATOR_PRIVATE_KEY --yes
+function bridge_and_post_metric() {
+    SUB_CMD=$1
+    CHAIN_ID=$2
+    AMOUNT=$3
 
-# Bridge back to L1. Account must be prefunded on mev-commit chain. 
-bridge-cli bridge-to-l1 890 $EMULATOR_ADDRESS $EMULATOR_PRIVATE_KEY --yes
+    bridge-cli $SUB_CMD $AMOUNT $EMULATOR_ADDRESS $EMULATOR_PRIVATE_KEY --yes
+    exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+        echo "Bridged $AMOUNT to Chain $CHAIN_ID successfully."
+        BRIDGING_SUCCESS=1
+    else
+        echo "Failed to bridge $AMOUNT to Chain $CHAIN_ID."
+        BRIDGING_SUCCESS=0
+    fi
+
+    dog --config /.dogrc metric post bridging.success $BRIDGING_SUCCESS --tags="amount_bridged:$AMOUNT,to_chain_id:$CHAIN_ID"
+}
+
+while true; do
+    # Generate a random amount between 0 and 10000 wei
+    RANDOM_AMOUNT=$(( (RANDOM % 10001) ))
+
+    bridge_and_post_metric "bridge-to-mev-commit" $MEV_COMMIT_CHAIN_ID $RANDOM_AMOUNT
+    bridge_and_post_metric "bridge-to-l1" $SEPOLIA_CHAIN_ID $RANDOM_AMOUNT
+    sleep 10
+done
