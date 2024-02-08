a24581278d722bbb6d352786f736642213105d52
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-02-07 17:53:10 -0800

    5 emulators working

--- standard/bridge-v1/emulator/main.go
+++ standard/bridge-v1/emulator/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"math/big"
-	"math/rand"
+	mathrand "math/rand"
+
 	"os"
 	"time"
 
@@ -21,7 +23,6 @@ const (
 	l1RPCUrl                     = "http://l1-bootnode:8545"
 	l1ContractAddrString         = "0x1a18dfEc4f2B66207b1Ad30aB5c7A0d62Ef4A40b"
 	settlementContractAddrString = "0xc1f93bE11D7472c9B9a4d87B41dD0a491F1fbc75"
-	bridgeIntervalSeconds        = 5
 )
 
 func main() {
@@ -51,11 +52,14 @@ func main() {
 	for {
 		// Generate a random amount of wei in [0, 10 ETH]
 		maxWei := new(big.Int).Mul(big.NewInt(10), big.NewInt(params.Ether))
-		randomWeiValue := big.NewInt(rand.Int63n(maxWei.Int64()))
+		randWeiValue, err := rand.Int(rand.Reader, maxWei)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to generate random value")
+		}
 
 		// Create and start the transfer to the settlement chain
 		tSettlement := transfer.NewTransferToSettlement(
-			randomWeiValue,
+			randWeiValue,
 			transferAddr,
 			privateKey,
 			settlementRPCUrl,
@@ -65,11 +69,16 @@ func main() {
 		)
 		tSettlement.Start(ctx)
 
-		time.Sleep(time.Second * bridgeIntervalSeconds)
+		// Sleep for random interval between 0 and 5 seconds
+		time.Sleep(time.Duration(mathrand.Intn(6)) * time.Second)
+
+		// Bridge back same amount minus 0.01 ETH for fees
+		pZeroOneEth := new(big.Int).Div(big.NewInt(params.Ether), big.NewInt(100))
+		amountBack := new(big.Int).Sub(randWeiValue, pZeroOneEth)
 
 		// Create and start the transfer back to L1 with the same amount
 		tL1 := transfer.NewTransferToL1(
-			randomWeiValue,
+			amountBack,
 			transferAddr,
 			privateKey,
 			settlementRPCUrl,
@@ -79,6 +88,7 @@ func main() {
 		)
 		tL1.Start(ctx)
 
-		time.Sleep(time.Second * bridgeIntervalSeconds)
+		// Sleep for random interval between 0 and 5 seconds
+		time.Sleep(time.Duration(mathrand.Intn(6)) * time.Second)
 	}
 }
