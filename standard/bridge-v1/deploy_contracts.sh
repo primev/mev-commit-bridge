d5585a93cbe9e44d414d4d0126883e914e10f1a0
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-02-28 22:22:45 -0800

    Squashed commit of the following:
    commit a62f80092565c7da5f6f0ada740bd8a6e8097bb9
    Author: Shawn <44221603+shaspitz@users.noreply.github.com>
    Date:   Wed Feb 28 22:13:55 2024 -0800
    
        introduce out path for artifacts
    
    commit dfceb711f2863be31a3782b10c3ce3bff38fff1c
    Author: Shawn <44221603+shaspitz@users.noreply.github.com>
    Date:   Wed Feb 28 22:06:09 2024 -0800
    
        awk deploy artifacts

--- standard/bridge-v1/deploy_contracts.sh
+++ standard/bridge-v1/deploy_contracts.sh
@@ -14,6 +14,9 @@ if [ ! -d "$CONTRACTS_PATH" ]; then
     exit 1
 fi
 
+ARTIFACT_OUT_PATH=${ARTIFACT_OUT_PATH:-"$HOME/.primev/contracts"}
+ARTIFACT_OUT_PATH=$(realpath "$ARTIFACT_OUT_PATH")
+
 fail_if_not_set() {
     if [ -z "$1" ]; then
         echo "Error: Required environment variable not set (one of SETTLEMENT_RPC_URL, L1_DEPLOYER_PRIVKEY, and RELAYER_ADDR)"
@@ -92,6 +95,8 @@ check_balance "$SETTLEMENT_RPC_URL" "$EXPECTED_WHITELIST_ADDR"
 echo "changing directory to $CONTRACTS_PATH and running deploy scripts for standard bridge"
 cd "$CONTRACTS_PATH" || exit
 
+# TODO: write to outpath (absolute?)
+
 RELAYER_ADDR="$RELAYER_ADDR" forge script \
     "scripts/DeployStandardBridge.s.sol:DeploySettlementGateway" \
     --rpc-url "$SETTLEMENT_RPC_URL" \
@@ -99,7 +104,9 @@ RELAYER_ADDR="$RELAYER_ADDR" forge script \
     --broadcast \
     --chain-id "$SETTLEMENT_CHAIN_ID" \
     -vvvv \
-    --use 0.8.23
+    --use 0.8.23 | tee deploy_sg_output.txt
+
+awk -F"JSON_DEPLOY_ARTIFACT: " '/JSON_DEPLOY_ARTIFACT:/ {print $2}' deploy_sg_output.txt | sed '/^$/d' > SettlementGatewayArtifact.json
 
 RELAYER_ADDR="$RELAYER_ADDR" forge script \
     "scripts/DeployStandardBridge.s.sol:DeployL1Gateway" \
@@ -108,4 +115,11 @@ RELAYER_ADDR="$RELAYER_ADDR" forge script \
     --broadcast \
     --chain-id "$L1_CHAIN_ID" \
     -vvvv \
-    --use 0.8.23
+    --use 0.8.23 | tee deploy_l1g_output.txt
+
+awk -F"JSON_DEPLOY_ARTIFACT: " '/JSON_DEPLOY_ARTIFACT:/ {print $2}' deploy_l1g_output.txt | sed '/^$/d' > L1GatewayArtifact.json
+
+mv SettlementGatewayArtifact.json "$ARTIFACT_OUT_PATH"
+mv L1GatewayArtifact.json "$ARTIFACT_OUT_PATH"
+
+rm deploy_sg_output.txt deploy_l1g_output.txt
